d2e2fcdfb1583850f0afc6bb6c08aecfb9c11a70
Felix.Euteneuer <Felix.Euteneuer@googlemail.com>
2014-11-26 16:43:45 +0100

    Abstand Rechts

---
+++ fw-motion/src/api/right_distance.h
@@ -0,0 +1,38 @@
+/**
+ * (c) Real-time systems project seminar, TU Darmstadt
+ * 
+ * @file right_distance.h
+ * @author Felix	
+ * @brief TODO - insert a brief description of that interface
+ **/
+
+#ifndef RIGHT_DISTANCE_H_
+#define RIGHT_DISTANCE_H_
+
+/*
+ * Include files
+ */
+#include "base_type.h"
+
+/*
+ * Global pre-processor symbols/macros ('#define')
+ */
+
+/*
+ * Global type definitions ('typedef')
+ */
+
+/*
+ * Global and local variable declarations
+ */
+
+int16_t right_distance;
+
+/*
+ * Global and local functions
+ */
+
+int16_t get_right_distance(void);
+void set_right_distance(int16_t);
+
+#endif /* RIGHT_DISTANCE_H_ */
