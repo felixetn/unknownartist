dcd977ed99d190edabf86bf9529a80ed8f5505c8
zydariv <daniel.alte92@yahoo.de>
2014-12-05 11:22:38 +0100

    gablung

---
+++ fw-motion/src/api/gablung.c
@@ -0,0 +1,64 @@
+/**
+ * (c) Real-time systems project seminar, TU Darmstadt
+ * 
+ * @file gablung.c
+ * @author Daniel	
+ * @brief TODO - insert a brief description of that module
+ **/
+
+/*
+ * Include files
+ */
+
+/*
+ * Global pre-processor symbols/macros ('#define')
+ */
+
+/*
+ * Global type definitions ('typedef')
+ */
+
+/*
+ * Global and local variable declarations
+ */
+
+/*
+ * Global and local functions
+ */
+
+void gablung_scan(){
+	/*int8_t navigation[3]; // kommt später in die Main
+
+	switch(navigation[0]){
+	case 1:
+		if(us_getRightDistance() > 200){
+
+		}
+		break;
+
+	case 2:
+		if(us_getFrontDistance() > 250){
+		}
+		break;
+
+	case 3:
+
+	if(us_getRightDistance() > 200){
+	}
+	break;
+
+	case default:	break;
+
+	}
+*/
+}
+
+/**
+ * @brief Put a message in the transmit buffer.
+ * @param param one
+ * @param param two
+ * @return result
+ */
+//uint8_t ModuleFoo_DooFoo(uint8_t* data, uint8_t len) {
+	//return 1;
+//}
---
+++ fw-motion/src/api/gablung.h
@@ -0,0 +1,42 @@
+/**
+ * (c) Real-time systems project seminar, TU Darmstadt
+ * 
+ * @file gablung.h
+ * @author Daniel	
+ * @brief TODO - insert a brief description of that interface
+ **/
+
+#ifndef GABLUNG_H_
+#define GABLUNG_H_
+
+/*
+ * Include files
+ */
+#include "api.c"
+#include "api.h"
+/*
+ * Global pre-processor symbols/macros ('#define')
+ */
+
+/*
+ * Global type definitions ('typedef')
+ */
+
+/*
+ * Global and local variable declarations
+ */
+
+/*
+ * Global and local functions
+ */
+
+/**
+ * @brief Put a message in the transmit buffer.
+ * @param param one
+ * @param param two
+ * @return result
+ */
+extern uint8_t ModuleFoo_DooFoo(uint8_t* data, uint8_t len);
+
+
+#endif /* GABLUNG_H_ */
